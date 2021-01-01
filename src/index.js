c8c363d12678f270de0452566e018ec9b5ff08b9
Kishare <kicshary@gmail.com>
2021-01-01 23:33:16 +0100

    chore(index): modifying structure

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import NavBar from './components/navbar';
+import EventForm from './components/event-form';
+import Events from './components/events';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const App = () => (
-  <div><NavBar /></div>
+  <div>
+    <div><NavBar /></div>
+    <div><EventForm /></div>
+    <div><Events /></div>
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
