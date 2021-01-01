dc328ef45743a458847562f0413510d321b09d23
Kishare <kicshary@gmail.com>
2021-01-02 00:09:11 +0100

    chore(maintain): deleting useless parts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import NavBar from './components/navbar';
 import EventForm from './components/event-form';
-import Events from './components/events';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
@@ -12,7 +11,6 @@ const App = () => (
   <div>
     <div><NavBar /></div>
     <div><EventForm /></div>
-    <div><Events /></div>
   </div>
 );
 
